2c1a689ff4413a48f40843349ea39cda3bb0bb32
MJ <56290324+mjneri@users.noreply.github.com>
2023-11-22 19:41:38 +0800

    Update steamdeck_pacman.sh

--- steamdeck_pacman.sh
+++ steamdeck_pacman.sh
@@ -5,7 +5,11 @@
 
 sudo steamos-readonly disable
 sudo pacman-key --init
-sudo pacman-key --populate archlinux
+#sudo pacman-key --populate archlinux
+
+# From comment by "cmhacks" in https://www.reddit.com/r/SteamOS/comments/180kg3c/after_the_355_update_everytime_i_try_to_install/
+# Running the command below seems to populate holo keys as well (not really sure what that means...)
+sudo pacman-key --populate
 
 # Do an apt-get update
 sudo pacman -Syy
