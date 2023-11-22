88d006540f905878cbdfe530e8de0e80f8727a10
MJ <56290324+mjneri@users.noreply.github.com>
2023-11-22 21:14:42 +0800

    Create holo_gccsetup.sh

---
+++ holo_gccsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# For setting up GCC on Steam Deck after a SteamOS update.
+# Main reference: https://www.reddit.com/r/SteamDeck/comments/ulalfn/comment/jmwd2g5/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+# Make sure to setup the pacman keys first - see steamdeck_pacman.sh
+
+sudo steamos-readonly disable
+sudo pacman -S gcc base-devel glibc linux-api-headers --noconfirm
+
+echo "Script done."
