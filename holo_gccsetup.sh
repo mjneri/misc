c64636b2c217562743ba24073c17c7e57e285a39
MJ <56290324+mjneri@users.noreply.github.com>
2024-03-12 18:32:27 +0800

    Update holo_gccsetup.sh

--- holo_gccsetup.sh
+++ holo_gccsetup.sh
@@ -4,6 +4,9 @@
 # Main reference: https://www.reddit.com/r/SteamDeck/comments/ulalfn/comment/jmwd2g5/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 # Make sure to setup the pacman keys first - see steamdeck_pacman.sh
 
+# Delete fakeroot.conf first since an error always appears (about fakeroot already exists... or something) when setting up gcc
+sudo rm /etc/ld.so.conf.d/fakeroot.conf
+
 sudo steamos-readonly disable
 sudo pacman -S gcc base-devel glibc linux-api-headers --noconfirm
 sudo steamos-readonly enable
